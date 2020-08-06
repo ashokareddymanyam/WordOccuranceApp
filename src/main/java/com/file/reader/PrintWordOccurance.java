a9438c4b0e6621ff12ea7ca3e891555fd44399e1
Ashoka Reddy Manyam <ashokareddy.manyam@ad.infosys.com>
2020-08-06 16:55:49 +0530

    using powermock tested

--- src/main/java/com/file/reader/PrintWordOccurance.java
+++ src/main/java/com/file/reader/PrintWordOccurance.java
@@ -11,12 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PrintWordOccurance {
 
@@ -26,6 +20,20 @@ public class PrintWordOccurance {
 		List<String> readAllLines = readFileInList("d:/text1.txt");
 		
 		Map<String, Integer> counterMap = new HashMap<>();
+		prepareCounterMap(readAllLines, counterMap);
+		Comparator<Entry<String, Integer>> comparator = Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed();
+	    //Comparator<Entry<Object, Object>> comparingByValue = Map.Entry.comparingByValue(Collections.reverseOrder());
+		counterMap.entrySet().stream().sorted(comparator).forEach(i->System.out.println(i.getKey() +":"+i.getValue()));
+	}
+
+	/**
+	 * Method to prepare counter map
+	 * 
+	 * 
+	 * @param readAllLines
+	 * @param counterMap
+	 */
+	public static Map<String, Integer> prepareCounterMap(List<String> readAllLines, Map<String, Integer> counterMap) {
 		readAllLines.forEach(i->{
 			String[] split = i.split(" ");
 			for(int j=0;j<split.length;j++) {
@@ -39,11 +47,16 @@ public class PrintWordOccurance {
 			}
 			
 		});
-		Comparator<Entry<String, Integer>> comparator = Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed();
-	    //Comparator<Entry<Object, Object>> comparingByValue = Map.Entry.comparingByValue(Collections.reverseOrder());
-		counterMap.entrySet().stream().sorted(comparator).forEach(i->System.out.println(i.getKey() +":"+i.getValue()));
+		return counterMap;
 	}
 	
+	/**
+	 * Read file
+	 * 
+	 * 
+	 * @param fileName
+	 * @return List<String>
+	 */
 	public static List<String> readFileInList(String fileName) { 
 	    List<String> lines = Collections.emptyList(); 
 	    try{ 
@@ -56,18 +69,36 @@ public class PrintWordOccurance {
 	    return lines; 
 	  } 
 	
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
 	public final String helloMethod() {
         return "Hello";
     }
 
+	/**
+	 * 
+	 */
 	public static void thirdMethod() {
 		
 	}
 
+	/**
+	 * 
+	 * @param anyString
+	 * @return
+	 */
 	public static String firstMethod(String anyString) {
 		return "xyz";
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public static String secondMethod() {
 		return "abc";
 	}
--- src/main/java/com/file/reader/WordOccurance.java
+++ src/main/java/com/file/reader/WordOccurance.java
@@ -2,10 +2,8 @@ package com.file.reader;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,18 +19,8 @@ public class WordOccurance {
 			  String st; 
 			  try {
 				while ((st = br.readLine()) != null) {
-					String[] words = st.split(" ");
-					for(int j=0;j<words.length;j++) {
-						String key = words[j].strip();
-						if(counterMap.containsKey(key)) {
-							Integer integer = counterMap.get(key);
-							counterMap.put(key,integer+1);
-						} else {
-							counterMap.put(key,1);
-						}
-					}
+					prepareCounterMap(counterMap, st);
 				}
-				
 				//Comparing map values to sort based on value desc;
 				Comparator<Entry<String, Integer>> comparator = Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed();
 				//Comparator<Entry<Object, Object>> comparingByValue = Map.Entry.comparingByValue(Collections.reverseOrder());
@@ -44,7 +32,24 @@ public class WordOccurance {
 			System.out.println("Exceptin message"+e1.getMessage());
 		} 
 		
-   } 
+   }
+
+	/**
+	 * @param counterMap
+	 * @param st
+	 */
+	private static void prepareCounterMap(Map<String, Integer> counterMap, String st) {
+		String[] words = st.split(" ");
+		for(int j=0;j<words.length;j++) {
+			String key = words[j].strip();
+			if(counterMap.containsKey(key)) {
+				Integer integer = counterMap.get(key);
+				counterMap.put(key,integer+1);
+			} else {
+				counterMap.put(key,1);
+			}
+		}
+	} 
 
 
 }
